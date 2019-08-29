6db1f0674c91d3f72ac8859fa9bb7e23ea71faa7
Brandon Bergenroth <bbergenroth@rti.org>
2019-08-29 16:02:30 -0400

    Setup schema, tables and test data

---
+++ db/migrations/V3__AddData.sql
@@ -0,0 +1,22 @@
+INSERT INTO nwm.parameters (name) VALUES ('streamflow');
+INSERT INTO nwm.features (id) VALUES 
+(9731244),
+(9731472),
+(9731482),
+(9731242),
+(9731470),
+(9731258),
+(9731264),
+(9731512),
+(9731236),
+(9731246),
+(9731476),
+(9731238),
+(9731262),
+(9731530),
+(9731468),
+(9731248),
+(9731250);
+
+INSERT INTO nwm.observations (feature_id, parameter_id, observed, value) VALUES
+(9731236,1,'1994-10-18 01:00:00',.01)
