1158f4d0062f051862cad4e7f5454d27cc528b8a
Adam Shelton <adamshelton@rti.org>
2019-08-29 14:09:37 -0400

    more examples

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
+from time import sleep
 app = FastAPI()
 
 
@@ -7,9 +9,17 @@ class HelloMessage(BaseModel):
     message: str
 
 
+class NumbersMessage(BaseModel):
+    numbers: List[int] = []
+
 @app.get('/hello')
-def say_hello(name: str):
+def say_hello(name: str =None):
     if name:
         return HelloMessage(message='Hello, {0}'.format(name))
     return HelloMessage(message='Hello, Who are you?')
 
+
+@app.get('/count')
+def count(num: int):
+    nums = [num for num in range(0, num)]
+    return NumbersMessage(numbers=nums)
